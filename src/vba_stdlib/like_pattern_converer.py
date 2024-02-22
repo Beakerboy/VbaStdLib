8f6c871c6f6a21d8c9807d679a58c52c51e58ecf
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-02-22 15:38:24 -0500

    Update like_pattern_converer.py

--- src/vba_stdlib/like_pattern_converer.py
+++ src/vba_stdlib/like_pattern_converer.py
@@ -1,5 +1,5 @@
-from antlr4_vba.vbaVisitor import vba_LikeVisitor
+from antlr4_vba.vbaVisitor import vba_likeVisitor
 
 
-class LikePatternConverter(vba_LikeVisitor):
+class LikePatternConverter(vba_likeVisitor):
     pass
