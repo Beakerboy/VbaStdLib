edccfa7ec8e0f1dd023480031e10410c25932b86
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-02-22 16:02:54 -0500

    Update literal_factory.py

--- src/vba_stdlib/literal_factory.py
+++ src/vba_stdlib/literal_factory.py
@@ -43,7 +43,7 @@ def literal_from_string(value: str) -> Any:
         return float(value)
     if value[-1] == '"' and value[0] == '"':
         return value[1:-1]
-    if (re.fullmatch("#" + date_pattern "#", value) or
+    if (re.fullmatch("#" + date_pattern + "#", value) or
             re.fullmatch("#" + time_pattern + "#", value)):
         return parse(value[1:-1])
     # assume non-quoted string.
