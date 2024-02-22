c091b9167f7882866980fefcb2e8db1b194d6f75
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-02-22 15:18:39 -0500

    Update literal_factory.py

--- src/vba_stdlib/literal_factory.py
+++ src/vba_stdlib/literal_factory.py
@@ -1,4 +1,5 @@
 import re
+from dateutil.parser import parse
 from typing import Any
 
 
@@ -7,14 +8,26 @@ def literal_from_string(value: str) -> Any:
     Create a VBA literal type from a string.
     """
     boolean_pattern = "TRUE|FALSE"
-    hex_pattern = "&H[0-9A-F]+"
+    hex_pattern = "&H[\dA-F]+"
     oct_pattern = "&[Oo]?[0-7]+"
-    dec_pattern = "[0-9]+"
+    dec_pattern = "\d+"
     date_pattern = "##"
     exp = "[DEde][+-]?\d\d*"
     float_pattern1 = "\.\d\d*(" + exp + ")?"
     float_pattern2 = "\d\d*" + exp
     float_pattern3 = "\d\d*\.(\d\d*)?(" + exp + ")?"
+
+    // Date Literal
+    sep = "([ ]+|([]*[/-][]*))"
+    eng_month = "(JANUARY|FEBRUARY|MARCH|APRIL|MAY|JUNE|JULY|AUGUST|SEPTEMBER|OCTOBER|NOVEMBER|DECEMBER)"
+    month_abv = "(JAN|FEB|MAR|APR|JUN|JUL|AUG|SEP|OCT|NOV|DEC)"
+    month_name = "(eng_month|month_abv)"
+    date_val_part = "(\d+|" + month_name + ")"
+    date = date_val_part + sep + date_val_part + "(" + sep + date_val_part + ")?"
+    time_pattern1 = "(\d+(AM|PM|A|P))"
+    time_pattern2 = "(\d+[ ]?[:.][ ]?\d+([ ]?[:.][ ]?\d+)?)"
+    time_pattern = "(" + time_pattern1 + "|" + time_pattern2 ")"
+    date_pattern = date + "([ ]+" + time_pattern + ")?"
     if re.fullmatch(boolean_pattern, value.upper()):
         return value.upper() == "TRUE"
     if re.fullmatch(hex_pattern, value):
@@ -30,3 +43,8 @@ def literal_from_string(value: str) -> Any:
         return float(value)
     if value[-1] == '"' and value[0] == '"':
         return value[1:-1]
+    if re.fullmatch("#" + date_pattern "#", value) or
+            re.fullmatch("#" + time_pattern + "#", value):
+        return parse(value[1:-1])
+    # assume non-quoted string.
+    return value
