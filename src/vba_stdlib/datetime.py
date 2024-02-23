90ba3c45b77852a38a6c0b4ab3006573f05a67bc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-02-23 08:27:00 -0500

    Create datetime.py

---
+++ src/vba_stdlib/datetime.py
@@ -0,0 +1,86 @@
+from datetime import datetime
+
+
+def dateadd(interval: str, number: int, date: datetime):
+    pass
+
+
+def datediff():
+    pass
+
+
+def datepart():
+    pass
+
+
+def dateserial():
+    pass
+
+
+def datevalue():
+    pas
+
+def day():
+    pass
+    
+
+def hour():
+    pass
+
+
+def minute():
+    pass
+
+
+def month():
+    pass
+    
+
+def second():
+    pass
+
+
+def timeserial():
+    pass
+
+
+def timevalue():
+    pass
+
+
+def weekday():
+    pass
+
+
+
+def year():
+    pass
+
+
+# Properties
+def calendar():
+    pass
+
+
+def date():
+    pass
+
+
+def date_s():
+    pass
+
+
+def now():
+    pass
+
+
+def time():
+    pass
+
+
+def time_s():
+    pass
+
+
+def timer():
+    pass
