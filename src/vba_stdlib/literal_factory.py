5fa90da72e32eca8562d67635124928793e8a3a7
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-02-22 20:42:24 -0500

    Update literal_factory.py

--- src/vba_stdlib/literal_factory.py
+++ src/vba_stdlib/literal_factory.py
@@ -45,6 +45,7 @@ def literal_from_string(value: str) -> Any:
         return float(value)
     if value[-1] == '"' and value[0] == '"':
         return value[1:-1]
+    raise Exception(date_pattern)
     if (re.fullmatch("#" + date_pattern + "#", value) or
             re.fullmatch("#" + time_pattern + "#", value)):
         return parse(value[1:-1])
