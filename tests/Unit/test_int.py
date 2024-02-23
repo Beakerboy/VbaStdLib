d167193095d6dca56fa2a8eefa56d3b651ae240f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-02-22 19:31:57 -0500

    Update test_int.py

--- tests/Unit/test_int.py
+++ tests/Unit/test_int.py
@@ -1,6 +1,11 @@
 from vba_stdlib.literal_factory import literal_from_string
 
 
+def test_boolean() -> None:
+    assert literal_from_string("true")
+    assert literal_from_string("False") == False
+    
+
 def test_decimal() -> None:
     assert literal_from_string("10") == 10
 
@@ -11,3 +16,9 @@ def test_hex() -> None:
 
 def test_oct() -> None:
     assert literal_from_string("&O12") == 10
+
+
+def test_float() -> None:
+    assert literal_from_string("10.") == 10.0
+    assert literal_from_string("3.14") == 3.14
+    assert literal_from_string(".5") == .5
