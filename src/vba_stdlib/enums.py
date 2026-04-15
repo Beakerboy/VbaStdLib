020550eba5469a4413cba17c27e43c5d0e33fbd8
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-15 10:24:15 -0400

    Update enums.py

--- src/vba_stdlib/enums.py
+++ src/vba_stdlib/enums.py
@@ -13,3 +13,26 @@ vb_thursday = 5
 vb_friday = 6
 vb_saturday = 7
 vb_usesystemdayofweek = 0
+
+# 6.1.1.16 VbVarType
+vbArray = 8192
+vbBoolean = 11
+vbByte = 17
+vbCurrency = 6
+vbDataObject = 13
+vbDate = 7
+vbDecimal = 14
+vbDouble = 5
+vbEmpty = 0
+vbError = 10
+vbInteger = 2
+vbLong = 3
+# defined only on implementations
+# that support a LongLong value type
+vbLongLong = 20
+vbNull = 1
+vbObject = 9
+vbSingle = 4
+vbString = 8
+vbUserDefinedType = 36
+vbVariant = 12
