6790ab6403720a9a15cb38df56a5e8937ff7f50c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-17 12:53:27 -0400

    Create interaction.py

---
+++ src/vba_stdlib/interaction.py
@@ -0,0 +1,30 @@
+from typing import Any
+from vba_stdlib.enums import VbCallType, VbMsgBoxResult
+
+
+class Interaction:
+
+    @staticmethod
+    def CallByName(object: Object, proc_name: str, call_type: VbCallType, args: list[Any]) -> Any:
+        pass
+
+    @staticmethod
+    def Choose(index, choice) -> Any:
+        pass
+
+    @staticmethod
+    def CreateObject() -> Object:
+        pass
+
+    @staticmethod
+    def MsgBox(prompt: Any, buttons, title, help_file, context) -> VbMsgBoxResult
+        print(prompt)
+        return VbMsgBoxResult.vbOK
+
+    @staticmethod
+    def DoEvents() -> None:
+        pass
+
+    @staticmethod
+    def Environ() -> None:
+        pass
