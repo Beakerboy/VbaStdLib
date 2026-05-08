77c7b91dff3c64abe73107555106d2aec58be040
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-08 13:52:06 -0400

    Update vba_error.py

--- src/vba_stdlib/Exceptions/vba_error.py
+++ src/vba_stdlib/Exceptions/vba_error.py
@@ -4,9 +4,9 @@ from typing import TypeVar
 T = TypeVar('T', bound='VBAError')
 
 
-class VBAError(Exception) -> None:
+class VBAError(Exception):
     """Base class for VBA-style errors."""
-    def __init__(self: T, number: int, message: str):
+    def __init__(self: T, number: int, message: str) -> None:
         self.number = number
         self.message = f"Run-time error '{number}': {message}"
         super().__init__(self.message)
@@ -15,16 +15,16 @@ class VBAError(Exception) -> None:
 T = TypeVar('T', bound='SubscriptOutOfRange')
 
 
-class SubscriptOutOfRange(VBAError) -> None:
+class SubscriptOutOfRange(VBAError):
     """Error 9: Occurs when an index is outside array bounds."""
-    def __init__(self: T):
+    def __init__(self: T) -> None:
         super().__init__(9, "Subscript out of range")
 
 
 T = TypeVar('T', bound='ArrayLockedError')
 
 
-class ArrayLockedError(VBAError) -> None:
+class ArrayLockedError(VBAError):
     """Error 10: Occurs when attempting to ReDim a locked array."""
-    def __init__(self: T):
+    def __init__(self: T) -> None:
         super().__init__(10, "This array is fixed or temporarily locked")
