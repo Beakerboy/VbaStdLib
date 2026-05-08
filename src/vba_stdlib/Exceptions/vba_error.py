f5f3decf08ec3531469984f09f6a62f9871339cb
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-08 13:44:51 -0400

    Update vba_error.py

--- src/vba_stdlib/Exceptions/vba_error.py
+++ src/vba_stdlib/Exceptions/vba_error.py
@@ -1,16 +1,30 @@
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='VBAError')
+
+
 class VBAError(Exception):
     """Base class for VBA-style errors."""
-    def __init__(self, number, message):
+    def __init__(self: T, number, message):
         self.number = number
         self.message = f"Run-time error '{number}': {message}"
         super().__init__(self.message)
 
+
+T = TypeVar('T', bound='SubscriptOutOfRange')
+
+
 class SubscriptOutOfRange(VBAError):
     """Error 9: Occurs when an index is outside array bounds."""
-    def __init__(self):
+    def __init__(self: T):
         super().__init__(9, "Subscript out of range")
+
+
+T = TypeVar('T', bound='ArrayLockedError')
+
 
 class ArrayLockedError(VBAError):
     """Error 10: Occurs when attempting to ReDim a locked array."""
-    def __init__(self):
+    def __init__(self: T):
         super().__init__(10, "This array is fixed or temporarily locked")
