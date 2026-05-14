3c6df6c6d437f1a2f2bbe8d569c784227e4c1718
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-14 14:45:25 -0400

    Update test_math.py

--- tests/Unit/test_math.py
+++ tests/Unit/test_math.py
@@ -1,9 +1,9 @@
-from vba_stdlib.math import math
+from vba_stdlib.math import Math
 from vba_types import VBADouble, VBAInteger
 
 
 def test_abs() -> None:
     input = VBAInteger(1)
-    assert math.abs(input) == input
+    assert Math.abs(input) == input
     input2 = VBAInteger(-1)
-    assert math.abs(input2) == input
+    assert Math.abs(input2) == input
