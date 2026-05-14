521afa1179a8da75349c44162adbb41f964e3087
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-14 14:53:14 -0400

    Enhance test_abs with result variable and type check
    Refactor test_abs to store result and check type

--- tests/Unit/test_math.py
+++ tests/Unit/test_math.py
@@ -4,6 +4,8 @@ from vba_types import VBADouble, VBAInteger
 
 def test_abs() -> None:
     input = VBAInteger(1)
-    assert Math.abs(input) == input
+    result = Math.abs(input)
+    assert result == input
+    assert isinstance(result, VBADouble)
     input2 = VBAInteger(-1)
     assert Math.abs(input2) == input
