14d2f7f2b022b5c93ffed0ff5e81eceb1ac65d0a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-15 15:24:47 -0400

    Update test_interaction.py

--- tests/Unit/test_interaction.py
+++ tests/Unit/test_interaction.py
@@ -3,6 +3,15 @@ from pytest_mock import MockerFixture
 
 def test_msgbox(mocker: MockerFixture) -> None:
     mock_print = mocker.patch('builtins.print')
+    mock_input = mocker.patch('builtins.input', return_value="")
+    result = Interaction.msgbox("hello")
+    assert result.value.value == 1
+    expected = "Microsoft Excel\n\nhello\nOK    Cancel"
+    mock_print.assert_called_with(expected)
+
+def test_msgbox2(mocker: MockerFixture) -> None:
+    mock_print = mocker.patch('builtins.print')
+    mock_input = mocker.patch('builtins.input', return_value="\t")
     result = Interaction.msgbox("hello")
     assert result.value.value == 1
     expected = "Microsoft Excel\n\nhello\nOK    Cancel"
