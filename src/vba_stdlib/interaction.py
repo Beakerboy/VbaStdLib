113e1dcb16807055a79cc75100abfd9091637351
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-15 09:48:26 -0400

    Fix typo in return type annotation for msgbox

--- src/vba_stdlib/interaction.py
+++ src/vba_stdlib/interaction.py
@@ -24,7 +24,7 @@ class Interaction:
                buttons: VbMsgBoxStyle = VbMsgBoxStyle.vbokonly,
                title: str = "",
                help_file: str = "",
-               context: int | None = None) -> VbMsgBoxResul:
+               context: int | None = None) -> VbMsgBoxResult:
         print(str(prompt))
         return VbMsgBoxResult.vbOK
 
