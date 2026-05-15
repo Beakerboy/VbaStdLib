b342fd2014f368ef18dba084e8430bf0d5ec19cb
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-15 15:45:14 -0400

    Update test_interaction.py

--- tests/Unit/test_interaction.py
+++ tests/Unit/test_interaction.py
@@ -2,17 +2,19 @@ from vba_stdlib.interaction import Interaction
 from vba_stdlib.enums import VbMsgBoxStyle, VbMsgBoxResult
 from pytest_mock import MockerFixture
 
+
 def test_msgbox(mocker: MockerFixture) -> None:
     mock_print = mocker.patch('builtins.print')
-    mock_input = mocker.patch('builtins.input', return_value="")
+    mocker.patch('builtins.input', return_value="")
     result = Interaction.msgbox("hello")
     assert result == VbMsgBoxResult.vbok
     expected = "Microsoft Excel\n\nhello\nOK"
     mock_print.assert_called_with(expected)
 
+
 def test_msgbox2(mocker: MockerFixture) -> None:
     mock_print = mocker.patch('builtins.print')
-    mock_input = mocker.patch('builtins.input', return_value="\t")
+    mocker.patch('builtins.input', return_value="\t")
     result = Interaction.msgbox("hello", VbMsgBoxStyle.vbokcancel)
     assert result == VbMsgBoxResult.vbcancel
     expected = "Microsoft Excel\n\nhello\nOK    Cancel"
