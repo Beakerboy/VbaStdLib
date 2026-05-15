991ba289a82cb6dbaacd27495eeb22dbe0cfb7b3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-15 10:11:06 -0400

    Update interaction.py

--- src/vba_stdlib/interaction.py
+++ src/vba_stdlib/interaction.py
@@ -5,21 +5,6 @@ from vba_stdlib.enums import VbCallType, VbMsgBoxResult, VbMsgBoxStyle
 class Interaction:
 
     @staticmethod
-    def callbyname(obj: object,
-                   proc_name: str,
-                   call_type: VbCallType,
-                   args: list[Any]) -> Any:
-        pass
-
-    @staticmethod
-    def choose(index: Any, choice: Any) -> Any:
-        pass
-
-    @staticmethod
-    def createobject() -> object:
-        pass
-
-    @staticmethod
     def msgbox(prompt: Any,
                buttons: VbMsgBoxStyle = VbMsgBoxStyle.vbokonly,
                title: str = "",
@@ -27,11 +12,3 @@ class Interaction:
                context: int | None = None) -> VbMsgBoxResult:
         print(str(prompt))
         return VbMsgBoxResult.vbok
-
-    @staticmethod
-    def doevents() -> None:
-        pass
-
-    @staticmethod
-    def environ() -> None:
-        pass
